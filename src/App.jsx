0bacf29cf1b321d2d0241eaceae4fe5da7007909
shiv <shivendrathakursst@gmail.com>
2024-08-14 21:26:33 +0530

    final

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import RollDice from './Components/RollDice'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+
+function App() {
+    return (
+        <div>
+              <RollDice />
+        </div>
+    );
+}
+
+export default App;
